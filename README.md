af09cb2be343adace62a87eba85cb53de08e5691
Evan Adler <evanbenadler@gmail.com>
2020-12-14 23:52:06 -0500

    l12 submission

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 minsynth
 ========
 
+# Fork of Minisynth for a CS 6120 assignment
+
+I attempted to extend Minisynth with floating point values and type coercion in
+order to try to synthesize the John Carmack inverse square root hack based on a
+five-iteration "blind" Newton Rhapson program. I extended all bit vectors to 32
+bits and modified the goal Z3 expression to require both programs to be
+approximately equal on all inputs, rather than completely equal.
+
+The results were... inconclusive. If I require both programs to differ by at
+most 0.1, Z3 fails in a couple of seconds. This is probably indicative of some
+buggy behavior with the type coercion. However, I figured that perhaps the
+quick square root inverse and the five-step newton square root inverse
+legitimately differ by more than 0.1 on some inputs. I tried requiring them to
+differ by at most 1, and I got a very similar result (i.e., quickly failing).
+Next, I tried requiring them to differ by at most 10. This time, Z3 stalled for
+over 15 minutes, and I ended up cancelling it.
+
+Command: `python ex2.py < sketches/fast_inverse_sqrt.txt`
+
+# Original Readme
+
 These are supporting materials for a lecture on program synthesis in the [Sketch][] tradition.
 
 Install [Z3][] with its Python 3 bindings. With Homebrew:
--- ex2.py
+++ ex2.py
@@ -18,19 +18,26 @@ GRAMMAR = """
   | term ">>" item      -> shr
   | term "<<" item      -> shl
 
-?item: NUMBER           -> num
+?item: INT              -> int
+  | FLOAT               -> float
   | "-" item            -> neg
+  | "int" item          -> intcast
+  | "float" item        -> floatcast
+  | "int2bv" item       -> int2bv
+  | "float2bv" item     -> float2bv
   | CNAME               -> var
   | "(" start ")"
 
 %import common.NUMBER
+%import common.INT
+%import common.FLOAT
 %import common.WS
 %import common.CNAME
 %ignore WS
 """.strip()
 
 
-def interp(tree, lookup):
+def interp(tree, lookup, z3_mode):
     """Evaluate the arithmetic expression.
 
     Pass a tree as a Lark `Tree` object for the parsed expression. For
@@ -39,8 +46,8 @@ def interp(tree, lookup):
 
     op = tree.data
     if op in ('add', 'sub', 'mul', 'div', 'shl', 'shr'):  # Binary operators.
-        lhs = interp(tree.children[0], lookup)
-        rhs = interp(tree.children[1], lookup)
+        lhs = interp(tree.children[0], lookup, z3_mode)
+        rhs = interp(tree.children[1], lookup, z3_mode)
         if op == 'add':
             return lhs + rhs
         elif op == 'sub':
@@ -54,17 +61,49 @@ def interp(tree, lookup):
         elif op == 'shr':
             return lhs >> rhs
     elif op == 'neg':  # Negation.
-        sub = interp(tree.children[0], lookup)
+        sub = interp(tree.children[0], lookup, z3_mode)
         return -sub
-    elif op == 'num':  # Literal number.
-        return int(tree.children[0])
+    elif op == 'int':  # Literal number.
+        if z3_mode:
+            return z3.BitVecVal(int(tree.children[0]), 32)
+        else:
+            return int(tree.children[0])
+    elif op == 'float':  # Literal number.
+        if z3_mode:
+            return z3.BitVecVal(float(tree.children[0]), 32)
+        else:
+            return float(tree.children[0])
     elif op == 'var':  # Variable lookup.
         return lookup(tree.children[0])
     elif op == 'if':  # Conditional.
-        cond = interp(tree.children[0], lookup)
-        true = interp(tree.children[1], lookup)
-        false = interp(tree.children[2], lookup)
+        cond = interp(tree.children[0], lookup, z3_mode)
+        true = interp(tree.children[1], lookup, z3_mode)
+        false = interp(tree.children[2], lookup, z3_mode)
         return (cond != 0) * true + (cond == 0) * false
+    elif op == "intcast":
+        child = interp(tree.children[0], lookup, z3_mode)
+        if z3_mode:
+            return z3.BV2Int(child)
+        else:
+            return int(child)
+    elif op == "floatcast":
+        child = interp(tree.children[0], lookup, z3_mode)
+        if z3_mode:
+            return z3.fpBVToFP(child, z3.Float32())
+        else:
+            return float(child)
+    elif op == "int2bv":
+        child = interp(tree.children[0], lookup, z3_mode)
+        if z3_mode:
+            return z3.Int2BV(child, 32)
+        else:
+            return child
+    elif op == "float2bv":
+        child = interp(tree.children[0], lookup, z3_mode)
+        if z3_mode:
+            return z3.fpToIEEEBV(child)
+        else:
+            return child
 
 
 def pretty(tree, subst={}, paren=False):
@@ -98,7 +137,7 @@ def pretty(tree, subst={}, paren=False):
     elif op == 'neg':
         sub = pretty(tree.children[0], subst)
         return '-{}'.format(sub, True)
-    elif op == 'num':
+    elif op == 'int' or op == 'float':
         return tree.children[0]
     elif op == 'var':
         name = tree.children[0]
@@ -108,6 +147,8 @@ def pretty(tree, subst={}, paren=False):
         true = pretty(tree.children[1], subst)
         false = pretty(tree.children[2], subst)
         return par('{} ? {} : {}'.format(cond, true, false))
+    elif op == 'floatcast' or op=='intcast' or op=='float2bv' or op=='int2bv':
+        return pretty(tree.children[0], subst, True)
 
 
 def run(tree, env):
@@ -116,7 +157,8 @@ def run(tree, env):
     `env` is a mapping from variable names to values.
     """
 
-    return interp(tree, lambda n: env[n])
+    print(tree)
+    return interp(tree, lambda n: env[n], False)
 
 
 def z3_expr(tree, vars=None):
@@ -135,11 +177,12 @@ def z3_expr(tree, vars=None):
         if name in vars:
             return vars[name]
         else:
-            v = z3.BitVec(name, 8)
+            # v = z3.BitVec(name, 8)
+            v = z3.BitVec(name, 32)
             vars[name] = v
             return v
 
-    return interp(tree, get_var), vars
+    return interp(tree, get_var, True), vars
 
 
 def solve(phi):
@@ -159,6 +202,10 @@ def model_values(model):
         d.name(): model[d]
         for d in model.decls()
     }
+
+# https://stackoverflow.com/questions/22547988/how-to-calculate-absolute-value-in-z3-or-z3py
+def abs(x):
+    return z3.If(x >= 0,x,-x)
 
 
 def synthesize(tree1, tree2):
@@ -180,7 +227,8 @@ def synthesize(tree1, tree2):
     # Formulate the constraint for Z3.
     goal = z3.ForAll(
         list(plain_vars.values()),  # For every valuation of variables...
-        expr1 == expr2,  # ...the two expressions produce equal results.
+        # expr1 == expr2,  # ...the two expressions produce equal results.
+        abs(expr1 - expr2) <= 10,
     )
 
     # Solve the constraint.
@@ -198,6 +246,7 @@ def ex2(source):
     print(pretty(tree1))
     print(pretty(tree2, model_values(model)))
 
+    # print(run(tree2, {'a': 3.0, 'h1': 15, 'h2': 1}))
 
 if __name__ == '__main__':
     ex2(sys.stdin.read())
